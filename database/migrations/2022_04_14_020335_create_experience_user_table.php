580b370f11d26247b529ff663fbaba88cbcaaa95
ivanalissetiyan <ivanalissetiyan1998@gmail.com>
2022-04-14 10:26:55 +0700

    add foreign keys done

--- database/migrations/2022_04_14_020335_create_experience_user_table.php
+++ database/migrations/2022_04_14_020335_create_experience_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateExprerienceUserTable extends Migration
+class CreateExperienceUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateExprerienceUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('exprerience_user', function (Blueprint $table) {
+        Schema::create('experience_user', function (Blueprint $table) {
             $table->id();
             $table->foreignId('detail_user_id')->nullable()->index('fk_experience_user_to_detail_user');
             $table->longText('experience');
@@ -29,6 +29,6 @@ class CreateExprerienceUserTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('exprerience_user');
+        Schema::dropIfExists('experience_user');
     }
 }
